e051bbe08dbd8d8d41a525738e1dda5c4cead2c8
makk985 <mayankk985@gmail.com>
2024-12-28 17:52:27 +0530

    fffff

--- nbs/settings.py
+++ nbs/settings.py
@@ -109,7 +109,7 @@ DATABASES =  {
        'default': dj_database_url.config(
            default=os.environ.get('DATABASE_URL'),
            conn_max_age=600
-       )
+       ),
        'sslmode': 'require'
    }
 
